6f6f294b01fc77a64160c7f071d3144954c0489f
Oliver Zilken <ozilken@gmail.com>
2012-04-20 15:11:23 +0200

    Iitial COmmit of skatweb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skatweb::Application.config.secret_token = 'c5de30fc5b4d8158d4275a6b71e05081d2d5db15828b6ce3ec8e42b6dfff71b438140fc14573369426ed95e125124a948b45f1151f0f240df06cae31e15f8101'
