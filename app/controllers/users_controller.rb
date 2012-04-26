1ca6351efb776d18f9875568332fa8cd38fb7eec
Oliver Zilken <ozilken@gmail.com>
2012-04-26 12:26:55 +0200

    Finished user model and secure password registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
+	    
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,14 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+
+  attr_accessor :password, :password_confirmation
+
+  attr_accessible :email, :name, :password, :password_confirmation
   validates :name,  presence: true, length: { maximum: 50 }
+  
+  before_save { |user| user.email = email.downcase }
+	
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
